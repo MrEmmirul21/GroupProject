421f7b8fa2233587cb02b0f71e052b6da25bff2a
Emmirul Haziq <92413479+MrEmmirul21@users.noreply.github.com>
2022-01-19 02:26:31 +0800

    haziq

--- RPSserver.py
+++ RPSserver.py
@@ -1,102 +1,118 @@
-import os
-import pickle
+# Imports
 import socket
-import sys
-from _thread import *
-import random
+import threading
 import time
 
-ip = socket.gethostbyname(socket.gethostname())
-port = 5698
-conection = True
-client_answers = {}
-client_info = {}
-server_marks = 0
-client_marks = 0
-totatl_client = 0
-client_1 = 0
-client_2 = 0
+# Creating socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    server.bind(('', port))
-    print("> Waiting for client(s)....")
-    server.listen()
-except socket.error as e:
-    print(f"> Error : {e} ")
-    quit()
+# Socket config
+host = '127.0.0.1'
+port = 5050
 
-#function
-def rps_s(connection, server_marks, client_marks):
-    connection.send("! | > WELCOME TO ROCK PAPER SCISSORS SERVER < | ! \n".encode("utf-8"))
+s.bind((host, port))
+s.listen(3)
 
+print("Waiting for players to join")
+
+# Keeping track of players
+players = []
+playerID = 0
+player1 = ""
+player2 = ""
+
+# Send data to specific player
+def send_to_player(data, currentPlayer):
+    for player in players:
+        if currentPlayer == player:
+            player.sendall(data.encode("utf-8"))
+
+# Send data to players but them self
+def send_to_players(data, currentPlayer):
+    for player in players:
+        if currentPlayer != player:
+            player.sendall(data.encode("utf-8"))
+
+# Send data to all players
+def send_to_all_players(data):
+    for player in players:
+        player.send(data.encode("utf-8"))
+
+# Find the winner
+def game_handling():
+    global player1, player2
+    if player1 != "" and player2 != "":
+        if player1 == "r":
+            if player2 == "r":
+                send_to_all_players("IT'S A DRAW!!")
+            if player2 == "s":
+                send_to_player("You Win!", players[0])
+                send_to_player("You Lose!", players[1])
+            if player2 == "p":
+                send_to_player("You Lose!", players[0])
+                send_to_player("You Win!", players[1])
+        if player1 == "s":
+            if player2 == "s":
+                send_to_all_players("IT'S A DRAW!!")
+            if player2 == "p":
+                send_to_player("You Win!", players[0])
+                send_to_player("You Lose!", players[1])
+            if player2 == "r":
+                send_to_player("You Lose!", players[0])
+                send_to_player("You Win!", players[1])
+        if player1 == "p":
+            if player2 == "p":
+                send_to_all_players("IT'S A DRAW!!")
+            if player2 == "r":
+                send_to_player("You Win!", players[0])
+                send_to_player("You Lose!", players[1])
+            if player2 == "s":
+                send_to_player("You Lose!", players[0])
+                send_to_player("You Win!", players[1])
+        player1 = ""
+        player2 = ""
+        # delay sending RESTARTGAME otherwise it will sometimes print it
+        time.sleep(0.5)
+        send_to_all_players("RESTARTGAME")
+
+# Thread handling
+def thread_handling(conn, currentPlayerID):
     while True:
-        name = connection.recv(1024).decode("utf-8")
-        connection.send(f"\n > Hi {name} \n".encode("utf-8"))
-        time.sleep(5)
+        try:
+            dataIn = conn.recv(1024).decode("utf-8")
+            global player1, player2
+            if dataIn:
+                if dataIn == "r" or dataIn == "s" or dataIn == "p":
+                    if currentPlayerID == 0:
+                        player1 = dataIn
+                        game_handling()
+                    if currentPlayerID == 1:
+                        player2 = dataIn
+                        game_handling()
+                else:
+                    conn.send("\n Not a correct input...".encode("utf-8"))
+        except:
+            global playerID
+            print("Player Disconnected: " + str(currentPlayerID))
+            players.pop(currentPlayerID)
+            playerID = currentPlayerID
+            break
 
-        choice = connection.recv(1024).decode("utf-8")
-        choices = ['R', 'P', 'S', "s", "p", "r"]        #choice from client
-        com_choice = random.choice(choices)             #choice from server
-
-        if choice in choices:
-            if choice == com_choice:
-                connection.send('\n > It"s a tie...\n'.encode("utf-8"))
-            elif choice == 'R':
-                if com_choice == 'P':
-                    connection.send('\n > You lose, sorry :(\n'.encode("utf-8"))
-                    server_marks += 1
-                elif com_choice == 'S':
-                    connection.send('\n > You won!!!!!\n'.encode("utf-8"))
-                    client_marks += 1
-            elif choice == 'P':
-                if com_choice == 'S':
-                    connection.send('\n > You lose, sorry :(\n'.encode("utf-8"))
-                    server_marks += 1
-                elif com_choice == 'R':
-                    connection.send('\n > You won !!!\n'.encode("utf-8"))
-                    client_marks += 1
-            elif choice == "S":
-                if com_choice == 'R':
-                    connection.send('\n > You lose, sorry :(\n'.encode("utf-8"))
-                    server_marks += 1
-                elif com_choice == 'P':
-                    connection.send('\n > You won!!!!\n'.encode("utf-8"))
-                    client_marks += 1
-            if choice == "R" or choice == "r":
-                choice = "Rock"
-            elif choice == "P" or choice == "p":
-                choice = "Paper"
-            elif choice == "S" or choice == "s":
-                choice = "Scissors"
-
-            if com_choice == "R" or com_choice == "r":
-                com_choice = "Rock"
-            elif com_choice == "P" or com_choice == "p":
-                com_choice = "Paper"
-            elif com_choice == "S" or com_choice == "s":
-                com_choice = "Scissors"
-            connection.send(f"\n > Your Choice : {choice} \n > Servers Choice : {com_choice} ".encode("utf-8"))
-        elif choice == "Q":
-            connection.send(" > Disconnecting server....".encode("utf-8"))
-            connection.send(" > Bye Bye ".encode("utf-8"))
-            sys.exit(connection)
-        else:
-            connection.send('\n > Invalid choice \n > try again..\n'.encode("utf-8"))
-        print(f"\n > Your Choice : {choice} \n > Servers Choice : {com_choice} ")
-        if choice < com_choice:
-            connection.send("\n > You are ahead of the server... \n > Keep up the good gaming....\n".encode("utf-8"))
-        elif choice > com_choice:
-            connection.send("\n > Come on you can do this a little bit more win....\n".encode("utf-8"))
-        elif choice == com_choice:
-            connection.send("\n > You and the server is tied....\n".encode("utf-8"))
-        return connection.send(
-            f"\n > Your marks : {client_marks} \n > Server marks : {server_marks} \n".encode("utf-8"))
-
+# Handling incoming connections
 while True:
-    connection, address = server.accept()
-    total_player += 1
-    print(" > New Connection !!!")
-    print(f" > Connection : {connection} ")
-    print(f" > Address :  {address} ")
-    start_new_thread(rps_s, (connection, server_marks, client_marks))
+    if playerID < 3:
+        conn, addr = s.accept()
+        print("Player connected from: " + str(addr))
+        players.append(conn)
+
+        # Threading
+        thread = threading.Thread(target=thread_handling, args=(conn, playerID))
+        thread.start()
+        
+        if len(players) == 2:
+            send_to_all_players("READY_TO_PLAY")
+
+        playerID += 1
+    else:
+        print("Can't have more players...")
+        break
